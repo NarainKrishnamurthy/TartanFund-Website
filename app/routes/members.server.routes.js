35c824e7e2b91dc1484f8bb60967c2fd5f0910b4
Narain Krishnamurthy <narainsk@cmu.edu>
2015-08-31 18:34:45 -0400

    made multiple styltic adjustments. Hid some dev-side APIs from prod env

--- app/routes/members.server.routes.js
+++ app/routes/members.server.routes.js
@@ -1,17 +1,22 @@
 'use strict';
 
-var members = require('../../app/controllers/members.server.controller');
+var members = require('../../app/controllers/members.server.controller'),
+    config = require('../../config/config');
 
 module.exports = function(app){
+    if (process.env.NODE_ENV === "development"){ 
+        app.route('/members')
+            .post(members.create)
+            .get(members.list);
 
-    app.route('/members')
-        .post(members.create)
-        .get(members.list);
-
-    app.route('/members/:memberId')
-        .get(members.read)
-        .put(members.update)
-        .delete(members.delete);
-
-    app.param('memberId', members.memberByID);
+        app.route('/members/:memberId')
+            .get(members.read)
+            .put(members.update)
+            .delete(members.delete);
+        
+        app.param('memberId', members.memberByID);    
+    } else if (process.env.NODE_ENV === 'production') {
+        app.route('/members')
+            .get(members.list);
+    }
 }
